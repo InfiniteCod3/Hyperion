bf51c8111f531c5cb0bf0e2fcdad245b4f80ebf9
AnAverageBeing <yatharthrathi.rathi@gmail.com>
2023-02-07 16:28:23 +0530

    wow

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ var (
 	ip       = flag.String("ip", "0.0.0.0", "sets ip")
 	port     = flag.Int("port", 25565, "sets port")
 	protocol = flag.Int("protcol", 761, "sets version protocol")
+	duration = flag.Int("duration", 600, "duration in sec")
+	perDelay = flag.Int("perDelay", 100, "connecion per delay")
+	delay    = flag.Int("delay", 0, "delay in sec")
 )
 
 func main() {
@@ -22,9 +25,9 @@ func main() {
 		Ip:       *ip,
 		Port:     *port,
 		Protocol: *protocol,
-		Duration: 600 * time.Second,
-		PerDelay: 1000,
-		Delay:    0,
+		Duration: time.Duration(*duration) * time.Second,
+		PerDelay: *perDelay,
+		Delay:    time.Duration(*delay),
 	})
 }
 
